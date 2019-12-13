337ef86f14e743174d3066ae0514975a20ca5658
zohaib younis <zabi.jarral08@gmail.com>
2019-12-13 21:08:54 +0100

    Basic libraries added

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+
+
+const app = express();
+
+app.use(bodyParser.json())
+
+
+app.get('/', (req, res, next) => {
+    res.send('Hello from Express Server')
+})
+
+
+app.listen(3000)
