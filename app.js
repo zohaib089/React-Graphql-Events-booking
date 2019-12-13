f292076b81b4bf266ba2ce31612f53e99fe26d25
zohaib younis <zabi.jarral08@gmail.com>
2019-12-13 22:59:01 +0100

    basic structure of Event and Create Event have been set up

--- app.js
+++ app.js
@@ -1,15 +1,68 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
+
 
 
 const app = express();
+const events = [];
 
 app.use(bodyParser.json())
 
 
-app.get('/', (req, res, next) => {
-    res.send('Hello from Express Server')
+app.use('/api', graphqlHttp({
+    schema: buildSchema(
+
+        `
+        type Event {
+            _id:ID!
+            title:String!
+            description:String!
+            price:Float!
+            date:String!
+        }
+
+        input EventInput {
+             title:String!
+            description:String!
+            price:Float!
+            date:String!
+        }
+
+        type RootQuery {
+            events: [Event!]!
+        }
+
+        type RootMutation {
+            createEvent(eventInput:EventInput):Event
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+        `
+    ),
+    rootValue: {
+        events: () => {
+            return events;
+        },
+        createEvent: (args) => {
+            const event = {
+                _id: Math.random().toString(),
+                title: args.eventInput.title,
+                description: args.eventInput.description,
+                price: +args.eventInput.price,
+                date: new Date().toISOString()
+            }
+            events.push(event);
+            return event;
+        }
+    },
+    graphiql: true
 })
+);
 
 
 app.listen(3000)
